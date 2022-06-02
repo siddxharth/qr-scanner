c4e9e81ceede2edc155c0cdc3908f78d6509b319
Siddharth Khatri <siddxharth@gmail.com>
2022-06-02 18:17:20 +0530

    Add download QR button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react';
 import GenerateQR from './GenerateQR'
+import { FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-	const [text, setText] = useState(0);
+	const [text, setText] = useState('');
+	const download = () => {
+        const canvas = document.getElementById('canvas');
+        var url = canvas.toDataURL("image/png");
+        var link = document.createElement('a');
+        link.download = 'qr-code.png';
+        link.href = url;
+        link.click();
+    }
+	const btn_status = `${text === '' ? "disabled" : ""}`
+	const clear_status = `${text === '' ? "disabled" : ""}`
 	return (
-		<>
-			<GenerateQR text={text}/>
-			<input onChange={(e) => setText(e.target.value)}/>
-		</>
+		<div className="d-flex justify-content-center align-items-center"  style={{minHeight: '100vh'}}>
+			<div className='d-flex justify-content-center row align-items-center'>
+				<GenerateQR text={text}/>
+				<FormControl value={text} className="mt-3" onChange={(e) => setText(e.target.value)} placeholder="Start entering text..." />
+				<div>
+					<Button className={`m-3 text-center ${btn_status}`} onClick={() => download()}>Download</Button>
+					<Button variant="secondary" className={`m-3 text-center ${clear_status}`} onClick={() => setText('')}>Clear</Button>
+				</div>
+			</div>
+		</div>
 	);
 }
 
--- src/components/GenerateQR.jsx
+++ src/components/GenerateQR.jsx
@@ -1,15 +1,22 @@
-import { QRCodeSVG } from 'qrcode.react';
 
+
+import { QRCodeCanvas } from 'qrcode.react';
 const GenerateQR = (props) => {
     return (
-        <div>
-            <QRCodeSVG
+        <div className='d-flex justify-content-center'>
+            <QRCodeCanvas
+                id='canvas'
                 value={props.text}
-                size={256}
+                size={1200}
                 level={"L"}
                 includeMargin={false}
-                renderas={"svg"}
-            />
+                renderas={"canvas"}
+                style={{
+                    maxHeight: '40vh',
+                    maxWidth: '40vh',
+                }}
+                className=""
+                />
         </div>
     );
 }
