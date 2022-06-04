3bab1b6b4e199109027cf7de5b15aa7f23421d33
Siddharth Khatri <siddxharth@gmail.com>
2022-06-04 09:39:40 +0530

    Add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react';
-import GenerateQR from './GenerateQR'
-import { FormControl, Button } from 'react-bootstrap';
+import React from 'react'
+import GenerateQR from './GenerateQR';
+import ScanQR from './ScanQR';
+import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Outlet } from 'react-router-dom';
 
-function App() {
-	const [text, setText] = useState('');
-	const download = () => {
-        const canvas = document.getElementById('canvas');
-        var url = canvas.toDataURL("image/png");
-        var link = document.createElement('a');
-        link.download = 'qr-code.png';
-        link.href = url;
-        link.click();
-    }
-	const btn_status = `${text === '' ? "disabled" : ""}`
-	const clear_status = `${text === '' ? "disabled" : ""}`
-	return (
-		<div className="d-flex justify-content-center align-items-center"  style={{minHeight: '100vh'}}>
-			<div className='d-flex justify-content-center row align-items-center'>
-				<GenerateQR text={text}/>
-				<FormControl value={text} className="mt-3" onChange={(e) => setText(e.target.value)} placeholder="Start entering text..." />
-				<div>
-					<Button className={`m-3 text-center ${btn_status}`} onClick={() => download()}>Download</Button>
-					<Button variant="secondary" className={`m-3 text-center ${clear_status}`} onClick={() => setText('')}>Clear</Button>
-				</div>
-			</div>
-		</div>
-	);
+export default function App() {
+  return (
+    <div className='w-100 col'>
+        <Link to="generate"><Button style={{minWidth: "50vw"}}>Generate QR</Button></Link>
+        <Link to="scan"><Button style={{minWidth: "50vw"}}>Scan QR</Button></Link>
+        <Outlet />
+    </div>
+  )
 }
-
-export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import GenerateQR from './components/GenerateQR';
+import ScanQR from './components/ScanQR';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<App />
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<App />}>
+				<Route path='generate' element={<GenerateQR />} />
+				<Route path='scan' element={<ScanQR />} />
+			</Route>
+		</Routes>
+	</BrowserRouter>
 );
